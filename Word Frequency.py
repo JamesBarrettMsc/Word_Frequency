a85b9a195a51c010581af5c1bfc8cee7f1bb392b
James Barrett <57573002+JamesBarrettMsc@users.noreply.github.com>
2025-05-09 15:10:09 +0100

    Update Word Frequency.py

--- Word Frequency.py
+++ Word Frequency.py
@@ -13,8 +13,16 @@ def get_frequency(text:str)->list[str,int]:
     
     return words_by_frequency
 
-
-
+def read_text_file(file_path:str)->str:
+    
+    text=""
+    try:
+        with open(file_path) as f:
+            text=f.read(file_path)
+    except Exception as e:
+        print(e)
+    else:
+        return file_path
 
 def main()->None:
     print(get_frequency("the quick brown fox jumped over the lazy dog"))
